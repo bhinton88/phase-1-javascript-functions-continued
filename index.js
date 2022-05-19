ce5431bd52207a1a243671a1f8fe3a75345a2db8
Bobby Hinton <bob.g.hint@gmail.com>
2022-05-19 11:53:58 -0500

    commit tree 4da54e2720269ff378ad4b65cda98fbe123444eb

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun (activity = `roller-skate`) {
+    return `This Saturday, I want to ${activity}!`; 
+}
+
+// function expression, setting a function to a variable 
+const mondayWork = function(activity = `go to the office`) {
+    return `This Monday, I will ${activity}.`; 
+}
+
+function wrapAdjective(adjective = `*`) {
+    const wrapFunction = function(basic =`special`) {
+        return `You are ${adjective}${basic}${adjective}!`;
+    }
+    return wrapFunction; 
+}
